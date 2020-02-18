de68676367986da72e994d32ceeebe8659949645
Anthony Kawa <anthonyk2020@gmail.com>
2020-02-17 16:33:07 -0800

    used reactstrap and bootstrap to build out card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import axios from "axios";
 import "./App.css";
 
+const API_KEY = "k0Pk1YxvkvaZ2agWoiM9efuyscQElxLQFVaX8W4g";
+const API_URL = "https://api.nasa.gov/planetary/apod";
+
 function App() {
+  const [imageData, setImageData] = useState();
+  const [todaysDate, setTodaysDate] = useState(
+    `${new Date().getFullYear()}-${new Date().getMonth() +
+      1}-${new Date().getDate()}`
+  );
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}?api_key=${API_KEY}&date=${todaysDate}`)
+      .then(res => {
+        console.log(res);
+        setImageData(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {imageData ? (
+        <Card>
+          <CardBody>
+          <CardTitle>
+              <h3>{imageData["title"]}</h3>
+            </CardTitle>
+            
+          </CardBody>
+          <CardImg
+            top
+            width="100%"
+            src={imageData["url"]}
+            alt={imageData["title"]}
+          />
+          <CardBody>
+          <CardTitle>Date: {todaysDate}</CardTitle>
+            <CardText>{imageData["explanation"]}</CardText>
+          </CardBody>
+        </Card>
+      ) : null}
     </div>
   );
 }
